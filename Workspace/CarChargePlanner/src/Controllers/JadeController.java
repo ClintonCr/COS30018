c79e1ccaa0ecbe8d8cd86b3f5d853f782a503905
jamahl.carter <jamahl.carter@enett.com>
2018-10-07 16:58:35 +1100

    Added JadeController and empty gateways

---
+++ Workspace/CarChargePlanner/src/Controllers/JadeController.java
@@ -0,0 +1,45 @@
+package Controllers;
+
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+
+import jade.wrapper.*;
+
+public class JadeController {
+	// Fields
+	private final CarGateway _carGateway;
+	private final MsaGateway _msaGateway;
+	private final ContainerController _mainContainer;
+	private final ContainerController _carContainer;
+	
+	// Constructors
+	public JadeController() {
+		_carGateway = new CarGateway();
+		_msaGateway = new MsaGateway();
+		_mainContainer = createContainer(null, true, true);
+		_carContainer = createContainer("car_container", false, false);
+	}
+	
+	// Methods
+	public void createCarAgent() {
+		
+	}
+	
+	// Helpers
+	private static ContainerController createContainer(String name, boolean showGui, boolean isMainContainer) {
+		Runtime rt = Runtime.instance();
+		Profile profile = new ProfileImpl(null, 8888, null);
+		
+		if (name != null && name != "")
+			profile.setParameter(Profile.CONTAINER_NAME, name);
+		
+		if (showGui)
+			profile.setParameter(Profile.GUI, "true");
+		
+		if (isMainContainer)
+			return rt.createMainContainer(profile);
+		
+		return rt.createAgentContainer(profile);
+	}
+}
