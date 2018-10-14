e71862dc69a6030b8b53c500c24b57040606c190
ClintonCr <clinton_2110@hotmail.com>
2018-10-14 14:17:14 +1100

    Basic car agent input added to UI

--- Workspace/CarChargePlanner/src/UI.java
+++ Workspace/CarChargePlanner/src/UI.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,10 @@ import jade.wrapper.StaleProxyException;
 
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 public class UI {
 	private JFrame frame;
@@ -58,9 +64,136 @@ public class UI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 600, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JButton btnAddButton = new JButton("New button");
-		frame.getContentPane().add(btnAddButton, BorderLayout.WEST);
+		//Car Type
+		
+		JComboBox cmbCarType = new JComboBox();
+		cmbCarType.setBounds(471, 232, 90, 16);
+		frame.getContentPane().add(cmbCarType);
+		
+		JLabel lblChargetype = new JLabel("Car Type:");
+		lblChargetype.setBounds(377, 232, 90, 16);
+		frame.getContentPane().add(lblChargetype);
+		
+		//Minimum Expected Charge
+		
+		JLabel lblMinCharge = new JLabel("Minimum Charge");
+		lblMinCharge.setBounds(377, 262, 90, 16);
+		frame.getContentPane().add(lblMinCharge);
+		
+		JTextField txtMinCharge = new JTextField("Min Charge");
+		txtMinCharge.setBounds(471, 262,  90,  16);
+		txtMinCharge.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent fe) {
+				
+				if(txtMinCharge.getText().isEmpty()) {
+					txtMinCharge.setText("Min Charge");
+				}
+			}
+			
+			@Override
+			public void focusGained(FocusEvent fe) {
+				
+				if (txtMinCharge.getText().equals("Min Charge")) {
+					txtMinCharge.setText("");
+				}
+			}
+		});
+		frame.getContentPane().add(txtMinCharge);
+		
+		//Maximum Expected Charge
+		
+		JLabel lblMaxCharge = new JLabel("Maximum Charge");
+		lblMaxCharge.setBounds(377, 292, 90, 16);
+		frame.getContentPane().add(lblMaxCharge);
+		
+		JTextField txtMaxCharge = new JTextField("Max Charge");
+		txtMaxCharge.setBounds(471, 292,  90,  16);
+		txtMaxCharge.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent fe) {
+				
+				if(txtMaxCharge.getText().isEmpty()) {
+					txtMaxCharge.setText("Max Charge");
+				}
+			}
+			
+			@Override
+			public void focusGained(FocusEvent fe) {
+				
+				if (txtMaxCharge.getText().equals("Max Charge")) {
+					txtMaxCharge.setText("");
+				}
+			}
+		});
+		frame.getContentPane().add(txtMaxCharge);
+		
+		//Start Time
+		
+		JLabel lblStartTime = new JLabel("Start Time");
+		lblStartTime.setBounds(377, 322, 90, 16);
+		frame.getContentPane().add(lblStartTime);
+		
+		JTextField txtStartTime = new JTextField("Enter Start Time");
+		txtStartTime.setBounds(471, 322,  90,  16);
+		txtStartTime.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent fe) {
+				
+				if(txtStartTime.getText().isEmpty()) {
+					txtStartTime.setText("Enter Start Time");
+				}
+			}
+			
+			@Override
+			public void focusGained(FocusEvent fe) {
+				
+				if (txtStartTime.getText().equals("Enter Start Time")) {
+					txtStartTime.setText("");
+				}
+			}
+		});
+		frame.getContentPane().add(txtStartTime);
+		
+		//Deadline
+		
+		JLabel lblDeadlineTime = new JLabel("Deadline");
+		lblDeadlineTime.setBounds(377, 352, 90, 16);
+		frame.getContentPane().add(lblDeadlineTime);
+		
+		JTextField txtDeadlineTime = new JTextField("Enter Deadline");
+		txtDeadlineTime.setBounds(471, 352,  90,  16);
+		txtDeadlineTime.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent fe) {
+				
+				if(txtDeadlineTime.getText().isEmpty()) {
+					txtDeadlineTime.setText("Enter Deadline");
+				}
+			}
+			
+			@Override
+			public void focusGained(FocusEvent fe) {
+				
+				if (txtDeadlineTime.getText().equals("Enter Deadline")) {
+					txtDeadlineTime.setText("");
+				}
+			}
+		});
+		frame.getContentPane().add(txtDeadlineTime);
+		
+		//Add Agent Button		
+		
+		JButton btnAddButton = new JButton("Add Car");
+		btnAddButton.setBounds(424, 377, 90, 28);
+		frame.getContentPane().add(btnAddButton);
+		
 		btnAddButton.addActionListener(new ActionListener() {
 			
 			@Override
